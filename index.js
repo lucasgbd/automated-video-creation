ab1adba7c9cff2b7a1a583c73d19ab6fc7188921
Lucas Gomes <lucasgbd@hotmail.com.br>
2019-05-19 16:12:00 +0100

    feat: add Watson natural language understanding service

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const robots = {
 }
 
 async function start() {
-    const content = {};
+    const content = {
+        maximumSentences: 7
+    };
 
     content.searchTerm = AskAndReturnSearchTerm();
     content.prefix = AskAndReturnPrefix();
@@ -23,7 +25,7 @@ async function start() {
         return selectedPrefixText;
     }
 
-    console.log(content);
+    console.log(JSON.stringify(content, null, 4));
 }
 
 start();
